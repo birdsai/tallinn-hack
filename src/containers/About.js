910c6943aa0e742570d1cbede5c8084059b11508
Madhusudhan Srinivasa <madhums8@gmail.com>
2018-06-09 17:13:46 +0300

    content

--- src/containers/About.js
+++ src/containers/About.js
@@ -6,7 +6,11 @@ class About extends Component {
   render() {
     return (
       <div>
-        About and Team content
+        <div className="container">
+          <h1>About</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit atque adipisci accusantium sit tenetur eveniet fuga voluptates a, officiis, iure. Sapiente, aut voluptas accusantium, ipsum veniam nostrum iste nesciunt quos! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit atque adipisci accusantium sit tenetur eveniet fuga voluptates a, officiis, iure. Sapiente, aut voluptas accusantium, ipsum veniam nostrum iste nesciunt quos!</p>
+          <br/>
+        </div>
         <div className="py-5 bg-light">
           <Subscribe />
         </div>

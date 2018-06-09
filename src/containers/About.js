68168cd42005ef79ca6b3530b79bab1fb9584577
Alex <alex@investly.co>
2018-06-09 16:54:32 +0300

    Added placeholders for about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Subscribe from '../components/Subscribe';
+import Faces from "../components/Faces";
 
 class About extends Component {
   render() {
     return (
       <div className={'text-center'}>
         About and Team content
+        <Faces />
         <div className="py-5 bg-light">
           <Subscribe />
         </div>
